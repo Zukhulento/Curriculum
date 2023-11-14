7b4883ccb7fae708d7b00126f5b8655f07bbc48c
Zukhulento <luis.matus.jr@gmail.com>
2023-11-14 11:56:48 -0600

    schema one

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,14 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors:{
+      "background":"#082032",
+      "text1":"#ffffff",
+      "text2":"fff2cc",
+      "background2":"#2C394B",
+      "itembg":"#334756",
+      "whiter":"FF4C29",
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
